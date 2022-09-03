790f2f13121e2ff45a643c306be8332befd10d7b
Justin Lim Zhe Shien <zlim029@e.ntu.edu.sg>
2022-09-03 22:20:30 +0800

    Display system uptime

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -97,7 +97,29 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  long systemUptime, idleTime;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if(stream.is_open()){
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> systemUptime;
+  }
+  return systemUptime; 
+}
+
+// string LinuxParser::Kernel() {
+//   string os, kernel, version;
+//   string line;
+//   std::ifstream stream(kProcDirectory + kVersionFilename);
+//   if (stream.is_open()) {
+//     std::getline(stream, line);
+//     std::istringstream linestream(line);
+//     linestream >> os >> version >> kernel;
+//   }
+//   return kernel;
+// }
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
