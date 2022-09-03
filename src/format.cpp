806b77bd5717d9864591101550311227c994852b
Justin Lim Zhe Shien <zlim029@e.ntu.edu.sg>
2022-09-03 17:35:27 +0800

    Format uptime display

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,21 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times) { 
+    int hours, minutes, seconds;
+
+    //  Get total hours
+    hours = times / (60 * 60);
+
+    //  Get leftover minutes by using total uptime minus hours (in seconds)
+    minutes = (times - hours * (60 * 60)) / 60;
+
+    // Get leftover minutes by using total uptime minus hours and minutes (in seconds)
+    seconds =  times - hours * (60 * 60) - minutes * 60;
+
+    string str = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);
+    return str;
+}
